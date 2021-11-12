6b789a5aa1ebeb4a146b26865444e203b59dd382
ItzArima <skaypaprilia@gmail.com>
2021-11-12 13:37:05 +0100

    impostate immagini dinamiche e junbotron

---
+++ src/components/product.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="product-container">
+      <img :src="thumb" alt="" srcset="">
+      <p>{{series}}</p>
+  </div>
+</template>
+
+<script>
+
+export default {
+    name: "product",
+
+    props : {
+        thumb : String,
+        series : String,
+    },
+}
+</script>
+
+<style>
+
+</style>
